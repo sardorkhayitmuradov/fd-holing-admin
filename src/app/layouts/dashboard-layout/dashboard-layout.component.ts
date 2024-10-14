459b4eddaf721d1b8eef92c4befe0ed64be4846e
s.xayitmuradov <s.xayitmuradov@apexbank.uz>
2024-10-14 19:59:25 +0500

    Added mock logic of log out and log in

--- src/app/layouts/dashboard-layout/dashboard-layout.component.ts
+++ src/app/layouts/dashboard-layout/dashboard-layout.component.ts
@@ -1,18 +1,24 @@
-import { CommonModule } from "@angular/common";
-import { Component } from "@angular/core";
-import { RouterLink, RouterLinkActive, RouterOutlet } from "@angular/router";
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import {
+  Router,
+  RouterLink,
+  RouterLinkActive,
+  RouterOutlet,
+} from '@angular/router';
 
 import {
   NzBreadCrumbComponent,
   NzBreadCrumbModule,
-} from "ng-zorro-antd/breadcrumb";
-import { NzIconModule } from "ng-zorro-antd/icon";
-import { NzLayoutModule } from "ng-zorro-antd/layout";
-import { NzMenuModule } from "ng-zorro-antd/menu";
-import { NzSliderModule } from "ng-zorro-antd/slider";
+} from 'ng-zorro-antd/breadcrumb';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzSliderModule } from 'ng-zorro-antd/slider';
+import { asapScheduler } from 'rxjs';
 
 @Component({
-  selector: "fd-dashboard-layout",
+  selector: 'fd-dashboard-layout',
   standalone: true,
   imports: [
     CommonModule,
@@ -26,9 +32,17 @@ import { NzSliderModule } from "ng-zorro-antd/slider";
     NzMenuModule,
     RouterOutlet,
   ],
-  templateUrl: "./dashboard-layout.component.html",
-  styleUrl: "./dashboard-layout.component.scss",
+  templateUrl: './dashboard-layout.component.html',
+  styleUrl: './dashboard-layout.component.scss',
 })
 export class DashboardLayoutComponent {
   public isCollapsed = false;
+
+  public readonly router = inject(Router);
+
+  public handleLogOut(): void {
+    asapScheduler.schedule(() => {
+      this.router.navigate(['/auth/login']);
+    }, 1000);
+  }
 }
--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -19,10 +19,11 @@ import {
 import { NzModalComponent } from 'ng-zorro-antd/modal';
 import { NzResultComponent } from 'ng-zorro-antd/result';
 import { NzTagComponent } from 'ng-zorro-antd/tag';
-import { Subscription } from 'rxjs';
+import { asapScheduler, Subscription } from 'rxjs';
 
 import { StringOrJsonPipe } from '@shared/pipes/string-or-json.pipe';
 import { FormGroupFrom } from '@shared/types/form-group.types';
+import { NzMessageService } from 'ng-zorro-antd/message';
 
 export interface ILoginForm {
   username: string;
@@ -57,22 +58,30 @@ export class LoginComponent implements OnInit, OnDestroy {
 
   public readonly fb = inject(FormBuilder);
   public readonly router = inject(Router);
+  public readonly message = inject(NzMessageService);
+
   // public readonly authService = inject(AuthorizationService);
+  public loginForm = this.fb.group<FormGroupFrom<ILoginForm>>({
+    username: ['Иван Иванович', [Validators.required]],
+    password: ['Pass1234_5', [Validators.required]],
+  });
+  private loginSub: Subscription | undefined;
 
   public ngOnInit(): void {
     console.log('LoginComponent initialized');
   }
 
-  public loginForm = this.fb.group<FormGroupFrom<ILoginForm>>({
-    username: ['', [Validators.required]],
-    password: ['', [Validators.required]],
-  });
-
-  private loginSub: Subscription | undefined;
-
   public login(): void {
     this.loginSub?.unsubscribe();
     this.loading = true;
+
+    if (this.loginForm.invalid) return;
+
+    asapScheduler.schedule(() => {
+      this.loading = false;
+      this.createBasicMessage();
+      void this.router.navigate(['/admin/documents']);
+    }, 2000);
     // this.loginSub = this.authService.login(this.loginForm.value as ILoginForm).subscribe({
     //   next: (): void => {
     //     void this.router.navigate(['/'])
@@ -87,5 +96,11 @@ export class LoginComponent implements OnInit, OnDestroy {
 
   public ngOnDestroy(): void {
     this.loginSub?.unsubscribe();
+  }
+
+  private createBasicMessage(): void {
+    this.message.create('success', 'Вы успешно вошли в систему!', {
+      nzDuration: 2000,
+    });
   }
 }
