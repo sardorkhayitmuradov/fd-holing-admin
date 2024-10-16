1eaa7adb9d52b7e49890952d7546006c7abebd23
sardorkhayitmuradov <sardorxayitmurodov@gmail.com>
2024-10-16 21:38:55 +0500

    Add pdf viewer

--- src/app/pages/main/document-list/document/document.component.ts
+++ src/app/pages/main/document-list/document/document.component.ts
@@ -1,12 +1,14 @@
-import { Component, ElementRef, inject, ViewChild } from '@angular/core';
+import { ChangeDetectorRef, Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 import { NzButtonComponent } from 'ng-zorro-antd/button';
 import { NzResultComponent } from 'ng-zorro-antd/result';
 import { NzQRCodeComponent } from 'ng-zorro-antd/qr-code';
-import { NzUploadChangeParam, NzUploadComponent } from 'ng-zorro-antd/upload';
+import { NzUploadComponent } from 'ng-zorro-antd/upload';
 import { NzIconDirective } from 'ng-zorro-antd/icon';
 import { NzMessageService } from 'ng-zorro-antd/message';
+import { FixMeLater, QRCodeModule } from 'angularx-qrcode';
+import { PdfViewerModule } from 'ng2-pdf-viewer';
 
 @Component({
   selector: 'fd-document',
@@ -18,56 +20,92 @@ import { NzMessageService } from 'ng-zorro-antd/message';
     NzQRCodeComponent,
     NzUploadComponent,
     NzIconDirective,
+    QRCodeModule,
+    PdfViewerModule,
   ],
   templateUrl: './document.component.html',
   styleUrl: './document.component.scss',
 })
 export class DocumentComponent {
-  @ViewChild('download', { static: false }) download!: ElementRef;
+  public qrCodeLink = 'https://fd-holding/documents';
+  public loading = false;
 
-  public isQrCodeLoading = false;
+  public originalPdfSrc: string | undefined = '';
+  public translatedPdfSrc: string | undefined = '';
 
   public readonly message = inject(NzMessageService);
+  public readonly cdr = inject(ChangeDetectorRef);
 
-  public downloadQrCode(): void {
-    const canvas = document
-      .getElementById('download')
-      ?.querySelector<HTMLCanvasElement>('canvas');
-    if (canvas) {
-      this.download.nativeElement.href = canvas.toDataURL('image/png');
-      this.download.nativeElement.download = 'ng-zorro-antd';
-      const event = new MouseEvent('click');
-      this.download.nativeElement.dispatchEvent(event);
+  public pdfReader = (type: 'translated' | 'original', pdfSrc: File): void => {
+    const fileReader = new FileReader();
+
+    fileReader.onload = (e) => {
+      if (type === 'translated')
+        this.translatedPdfSrc = e.target?.result as string;
+      if (type === 'original') this.originalPdfSrc = e.target?.result as string;
+    };
+
+    fileReader.readAsArrayBuffer(pdfSrc);
+    this.cdr.detectChanges();
+  };
+
+  public onUploadChange(
+    type: 'translated' | 'original',
+    selectedFile: FixMeLater,
+  ): void {
+    console.log(selectedFile.file);
+    const isPdf = selectedFile.type === 'application/pdf';
+    const isLt2M = selectedFile.size! / 1024 / 1024 < 2;
+
+    if (!isPdf) {
+      this.message.error('You can only upload PDF file!');
     }
+
+    if (!isLt2M) {
+      this.message.error('Image must smaller than 2MB!');
+    }
+
+    this.pdfReader(type, selectedFile.file.originFileObj);
   }
 
-  public handleUploadOriginalChange({
-    file,
-    fileList,
-  }: NzUploadChangeParam): void {
-    const status = file.status;
-    if (status !== 'uploading') {
-      console.log(file, fileList);
-    }
-    if (status === 'done') {
-      this.message.success(`${file.name} file uploaded successfully.`);
-    } else if (status === 'error') {
-      this.message.error(`${file.name} file upload failed.`);
-    }
+  public deleteSelectedPdf(type: 'translated' | 'original'): void {
+    if (type === 'translated') this.translatedPdfSrc = '';
+    if (type === 'original') this.originalPdfSrc = '';
   }
 
-  public handleUploadTranslatedChange({
-    file,
-    fileList,
-  }: NzUploadChangeParam): void {
-    const status = file.status;
-    if (status !== 'uploading') {
-      console.log(file, fileList);
+  // qr code
+  public downloadQrCode(qrCode: FixMeLater): void {
+    const parentElement =
+      qrCode.qrcElement.nativeElement.querySelector('img').src;
+
+    console.log(parentElement);
+
+    let blobData = this.convertBase64ToBlob(parentElement);
+    // saves as image
+    const blob = new Blob([blobData], { type: 'image/png' });
+    const url = window.URL.createObjectURL(blob);
+    const link = document.createElement('a');
+    link.href = url;
+    // name of the file
+    link.download = 'angularx-qrcode';
+
+    link.click();
+  }
+
+  private convertBase64ToBlob(Base64Image: string) {
+    // split into two parts
+    const parts = Base64Image.split(';base64,');
+    // hold the content type
+    const imageType = parts[0].split(':')[1];
+    // decode base64 string
+    const decodedData = window.atob(parts[1]);
+    // create unit8array of size same as row data length
+    const uInt8Array = new Uint8Array(decodedData.length);
+    // insert all character code into uint8array
+    for (let i = 0; i < decodedData.length; ++i) {
+      uInt8Array[i] = decodedData.charCodeAt(i);
     }
-    if (status === 'done') {
-      this.message.success(`${file.name} file uploaded successfully.`);
-    } else if (status === 'error') {
-      this.message.error(`${file.name} file upload failed.`);
-    }
+    // return blob image after conversion
+    return new Blob([uInt8Array], { type: imageType });
   }
 }
